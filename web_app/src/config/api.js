892d326b23f3cde77ed66c978e7894d0d75d07e4
Nea-web <nea.web.agency@gmail.com>
2026-06-18 22:21:40 +0200

    movo1

--- web_app/src/config/api.js
+++ web_app/src/config/api.js
@@ -1,2 +1,3 @@
-export const API_URL = '/api';
-export const SOCKET_URL = window.location.origin;
+const BACKEND_URL = import.meta.env.VITE_API_URL || ''
+export const API_URL = BACKEND_URL ? `${BACKEND_URL}/api` : '/api'
+export const SOCKET_URL = BACKEND_URL || window.location.origin
